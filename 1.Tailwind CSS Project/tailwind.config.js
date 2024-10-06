ca06b3251f0f87bbbf631969325207e6424aff51
Farhana Bente Islam <103626684+farhanaiubat95@users.noreply.github.com>
2024-10-06 19:48:38 +0600

    Create a Tailwind css project

---
+++ 1.Tailwind CSS Project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        myriad: ['Myriad-Pro', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
